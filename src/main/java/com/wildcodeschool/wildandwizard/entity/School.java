fc52877c034af7352eb2080272b75ddaa139e4a4
Michael Obst <michael.obst@telekom.de>
2020-01-25 22:13:27 +0100

    Quest completed: Many2One-relationship established in entities Wizard to School

--- src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
@@ -1,9 +1,14 @@
 package com.wildcodeschool.wildandwizard.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+
 
 @Entity
 public class School {
@@ -14,9 +19,30 @@ public class School {
     private String name;
     private Long capacity;
     private String country;
+    
+    @OneToMany(mappedBy = "school")
+    private List<Wizard> wizards;
 
-    public School() {
+
+
+	public School() {
     }
+	
+    /**
+     * getter-method is neccessary for resolve one-to-many relationship school - wizard
+	 * @return the wizards
+	 */
+	public List<Wizard> getWizards() {
+		return wizards;
+	}
+
+//	setter-method isn't necessary in this example, since the wizards are selected in the database 
+//	/**
+//	 * @param wizards the wizards to set
+//	 */
+//	public void setWizards(List<Wizard> wizards) {
+//		this.wizards = wizards;
+//	}
 
     public Long getId() {
         return id;
--- src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
@@ -1,9 +1,14 @@
 package com.wildcodeschool.wildandwizard.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import java.sql.Date;
 
 @Entity
@@ -18,9 +23,30 @@ public class Wizard {
     private String birthPlace;
     private String biography;
     private boolean muggle;
+    
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH, optional = false)
+    @JoinColumn(name = "school_id", nullable = false)
+    private School school;
 
-    public Wizard() {
+
+	public Wizard() {
     }
+	
+//	getter-method isn't necessary in this example, since only the direction school -> wizards is fetched
+//  /**
+//	 * @return the school
+//	 */
+//	public School getSchool() {
+//		return school;
+//	}
+
+	/**
+	 * setter-method is necessary to resolve the many-to-one-relationship wizard-school
+	 * @param school the school to set
+	 */
+	public void setSchool(School school) {
+		this.school = school;
+	}
 
     public Long getId() {
         return id;
